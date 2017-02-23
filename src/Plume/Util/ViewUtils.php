f30069e698aaa8937b94c80836e1d0a1e1ef2080
zhangbaitong <zhangbaitong@163.com>
2017-02-23 17:06:51 +0800

    fix echo as method on php5.6

--- src/Plume/Util/ViewUtils.php
+++ src/Plume/Util/ViewUtils.php
@@ -4,11 +4,11 @@ namespace Plume\Util;
 
 class ViewUtils {
 	
-	public static function echo($data){
+	public static function write($data){
         echo $data;
     }
 
-    public static function safeEcho($data){
+    public static function safeWrite($data){
     	echo isset($data) ? $data : '';
     }
 }
