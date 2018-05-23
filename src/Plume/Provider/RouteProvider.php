1cd9bcd7cf4092b7fe541e1aa2badc9850157aed
zhanghw <zhanghw@infobird.com>
2018-05-23 15:20:09 +0800

    ucfirst

--- src/Plume/Provider/RouteProvider.php
+++ src/Plume/Provider/RouteProvider.php
@@ -37,7 +37,7 @@ class RouteProvider extends Provider{
 		    $action = ArrayUtils::getValue($reqPathArr, 3, 'index');
 	    }else{
 		    //使用模块名前缀配置，使页面访问路径缩短为二级路径
-		    $module = ArrayUtils::getValue($prefixArr, 0, $this->plume('plume.module.default'));
+		    $module = ucfirst(ArrayUtils::getValue($prefixArr, 0, $this->plume('plume.module.default')));
 		    $controller = ucfirst(ArrayUtils::getValue($prefixArr, 1, 'index'));
 		    $action = ArrayUtils::getValue($prefixArr, 2, 'index');
 	    }
