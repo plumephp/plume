b6a14cefa3ea7522486c34ece8921f1fbc1a5250
jiangyao <jiangyao@infobrid.com>
2018-07-17 17:50:56 +0800

    support multi database

--- src/Plume/Core/Dao.php
+++ src/Plume/Core/Dao.php
@@ -13,22 +13,24 @@ class Dao{
     protected $tableName = null;
     protected $tableId = null;
     protected $classType = 'dao';
+    protected $db = 'db';
 
-    public function __construct($app, $tableName, $tableId = 'id'){
+    public function __construct($app, $tableName, $tableId = 'id', $db = 'db'){
         $this->app = $app;
         $this->tableName = $tableName;
         $this->tableId = $tableId;
+        $this->db = $db;
     }
 
     public function getTableName(){
-    	return $this->tableName;
+        return $this->tableName;
     }
 
     public function getTableId(){
-    	return $this->tableId;
+        return $this->tableId;
     }
 
     public function connect(){
-        return $this->provider('dataBase')->connect();
+        return $this->provider('dataBase')->connect($this->db);
     }
 }
--- src/Plume/Provider/DataBaseProvider.php
+++ src/Plume/Provider/DataBaseProvider.php
@@ -6,27 +6,28 @@ use MysqliDb;
 
 class DataBaseProvider extends Provider{
 
-	private $con = null;
+    private $con = array();
 
-	public function connect(){
-		if(is_null($this->con)){
-			$config = $this->getConfig()['db'];
-			$mysqliDb = new MysqliDb(
-				$config['host'], $config['username'],
-				$config['password'], $config['database'],
-				(int)$config['port'], $config['charset']
-			);
-			$this->con = $mysqliDb->getInstance();
-		}
-		return $this->con;
-	}
+    public function connect($db = 'db'){
+        if(!isset($this->con[$db])){
+            $config = $this->getConfig()[$db];
+            $mysqliDb = new MysqliDb(
+                $config['host'], $config['username'],
+                $config['password'], $config['database'],
+                (int)$config['port'], $config['charset']
+            );
+            $this->con[$db] = $mysqliDb->getInstance();
+        }
+        return $this->con[$db];
+    }
 
-	public function close(){
-		if(!is_null($this->con)){
-			if($this->con instanceof MysqliDb){
-				$this->con->disconnect();
-			}
-			$this->con = null;
-		}
-	}
+    public function close($db = 'db'){
+        if(isset($this->con[$db])){
+            if($this->con[$db] instanceof MysqliDb){
+                $this->con[$db]->disconnect();
+            }
+            $this->con[$db] = null;
+            unset($this->con[$db]);
+        }
+    }
 }
